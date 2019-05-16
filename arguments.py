70cc0d037c2cc329e60ffb25d29ce1de03838953
Murilo Ferreira <fferreira.murilo@gmail.com>
2019-05-15 23:17:41 -0300

    Percentages will be inputed as ints, not floats

--- arguments.py
+++ arguments.py
@@ -5,9 +5,9 @@ def get_arguments():
     parser = argparse.ArgumentParser(description=help_str)
     # Default path is the current folder.
     parser.add_argument("path", type=str)
-    parser.add_argument('-T', "--train", type=float, default=None)
-    parser.add_argument('-t', "--test", type=float, default=None)
-    parser.add_argument('-V', "--val", type=float, default=None)
+    parser.add_argument('-T', "--train", type=int, default=0)
+    parser.add_argument('-t', "--test", type=int, default=0)
+    parser.add_argument('-V', "--val", type=int, default=0)
     # Return it as a dict
-    return vars(parser.parse_args())
+    return parser.parse_args()
 
