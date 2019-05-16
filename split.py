27052b21553bd9360ed295fc7aba6918e36a8859
Murilo Ferreira <fferreira.murilo@gmail.com>
2019-05-15 23:20:07 -0300

    Prototyped high level program

--- split.py
+++ split.py
@@ -1,11 +1,24 @@
 import arguments
-
+import dir_utils as utils
 
 
 
 def main():
     args = arguments.get_arguments()
-    print(args)
+    split_ratio = calculate_split(args)
+    classes = utils.list_dirs(args.path)
+    #TODO: funct that creates train-test-val folders and classes inside
+    #TODO: funct that moves right amount of data to each folder
+    
 
+def calculate_split(args):
+    ratio_sum = args.train + args.test + args.val
+    if 0 <= ratio_sum <= 100: 
+        #TODO: Decide what to do w/
+        #missing args
+    else:
+        print("Invalid combination of arguments!")
+        exit(1)
+    
 if __name__ == '__main__':
     main()
