ab0eb7bed6c2734666ad949a442fbc8031503b3c
Murilo Ferreira <fferreira.murilo@gmail.com>
2019-06-01 17:46:34 -0300

    --ratio is optional and has a default value

--- arguments.py
+++ arguments.py
@@ -4,7 +4,9 @@ def get_arguments():
     help_str = "Splits your dataset intro train-test-validate subfolders!"
     parser = argparse.ArgumentParser(description=help_str)
     parser.add_argument("path", type=str)
-    parser.add_argument("ratio", nargs='+', type=float)
+    parser.add_argument("-r", "--ratio", nargs='+', default=[.6,.2,.2], type=float)
+    #parser.add_argument("-ns", "--noshuffle")
+    #parser.add_argument("-c", "--copy") 
     # Return it as a dict
     return parser.parse_args()
 
