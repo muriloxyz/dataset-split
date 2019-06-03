6f7827fd33209dc9cd48e7ccc0cb64fece7e6848
Murilo Ferreira <fferreira.murilo@gmail.com>
2019-06-02 22:55:00 -0300

    Docstrings for split.py

--- split.py
+++ split.py
@@ -8,14 +8,25 @@ from math import ceil, floor
 # convention: args come in (train, val, test) format
 
 def replicate_classes(path, classes, subdirs):
+    '''
+    Will create all classes (dirs) inside every
+    subdir found in path. 
+    '''
     for subd in subdirs:
         utils.create_dirs(osp.join(path, subd), classes)
 
 def calculate_splits(items, folders):
+    '''
+    With a list of files (items) and numbers for folders ratios,
+    shuffles (if requested) the files and decides their final
+    folder in the split.
+    '''
     shuffle(items)
     n = len(items)
+    #Calculating where the list will be sliced
     train_limit = ceil(folders['train'] * n)
     test_limit = train_limit + floor(folders['test'] * n)
+    #Sliced list for every subset
     split = {'train': items[:train_limit],
              'valid': items[train_limit:test_limit],
              'test': items[test_limit:]
@@ -23,12 +34,20 @@ def calculate_splits(items, folders):
     return split
 
 def move_data(path, splits, subd):
+    '''
+    With the split already decided, this function
+    moves the files to their respective destination.
+    '''
     for d in splits.keys():
         origin = osp.join(path, subd)
         dest = osp.join(path, d, subd)
         utils.move_files(origin, dest, splits[d])
 
 def split_data(path, classes, new_folders):
+    '''
+    Calculates the split for traint-valid-test dirs
+    and moves the right amount of files.
+    '''
     for subd in classes:
         temp_path = osp.join(path, subd)
         items = [f for f in os.listdir(temp_path)]
@@ -36,6 +55,9 @@ def split_data(path, classes, new_folders):
         move_data(path, splits, subd)
 
 def main():
+    '''
+    To be added
+    '''
     args = arguments.get_arguments()
     ratio = args.ratio
     # Customized path 
