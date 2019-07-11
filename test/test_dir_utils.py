d7fb0cd1657fc45a9a154ea0f1660e83643bfa68
Murilo <fferreira.murilo@gmail.com>
2019-07-10 21:05:57 -0300

    Adds some tests to dir_utils.py

---
+++ test/test_dir_utils.py
@@ -0,0 +1,29 @@
+import pytest
+import os
+import os.path as osp
+import shutil as sh
+import dir_utils
+
+TEST_PATH = osp.join(os.getcwd(), 'test')
+TEST_DIRS = ['OMG', 'ROFL', 'XOXO', '.SNEAKY']
+
+def test_list_dirs():
+    expected1 = {'Apple', 'Pineapple', 'Orange'}
+    expected2 = {'Spaghetti', 'Steak', 'Rice'}
+    path = osp.join(TEST_PATH, 'test-directories')
+    listed_dirs1 = dir_utils.list_dirs(osp.join(path, 'folder1'))
+    listed_dirs2 = dir_utils.list_dirs(osp.join(path, 'folder2'))
+    assert len(expected1) == len(listed_dirs1)
+    assert len(expected2) == len(listed_dirs2)
+    assert expected1 == set(listed_dirs1)
+    assert expected2 == set(listed_dirs2)
+
+def test_create_dirs():
+    path = osp.join(TEST_PATH, 'test-directories')
+    dir_utils.create_dirs(path, TEST_DIRS)
+    listed_dirs = [d for d in os.listdir(path)]
+    expected = set(TEST_DIRS).union({'folder1', 'folder2'})
+    assert len(expected) == len(listed_dirs)
+    assert expected == set(listed_dirs)
+
+
