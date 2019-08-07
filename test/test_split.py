69025b50ab964ace021a0366ad671317551acf21
Murilo Ferreira <fferreira.murilo@gmail.com>
2019-08-06 23:55:04 -0300

    Continues development of tests for split.py

--- test/test_split.py
+++ test/test_split.py
@@ -2,13 +2,14 @@ import pytest
 import os
 import os.path as osp
 import shutil as sh
-import dir_utils
+import split as sp
 
 COPY_FOLDER = '_split_'
 THIS_PATH = osp.join(os.getcwd(), 'test')
 SAFE_PATH = osp.join(THIS_PATH, 'test-dataset')
 TEST_PATH = osp.join(THIS_PATH, 'test-dataset-exec')
 TEST_COPY_PATH = osp.join(TEST_PATH, COPY_FOLDER)
+GENERATED_DIRS = ['test', 'valid', 'train']
 RATIO = (.6, .2, .2) 
 
 
@@ -20,17 +21,35 @@ def clean_mess():
     #After each function delete the test folder
     sh.rmtree(TEST_PATH)
 
-def list_items():
-    # List all items inside the dataset (orders by class)
-    classes = [c for c in os.listdir(TEST_PATH) if osp.isdir(osp.join(TEST_PATH, c))]
-    items = [os.listdir(osp.join(TEST_PATH, c)) for c in classes]
+def list_dirs(path):
+    return [d for d in os.listdir(path) if osp.isdir(osp.join(path, d))]
+
+def list_items(path):
+    # List all items inside the dataset (for each class)
+    classes = list_dirs(path)
+    items = [os.listdir(osp.join(path, c)) for c in classes]
     return dict(zip(classes, items))
 
+def quantities_check(before, after):
+    pass
 
 def test_split_copy():
     # Tests split when copying files
-    pass
+    original_items = list_items(TEST_PATH)
+    sp.split(RATIO, TEST_PATH, True)
+    # Check: Copy directory created
+    assert COPY_FOLDER in os.listdir(TEST_PATH)
+    # Check: The number of directories increased by one (split folder)
+    assert len(list_dirs(TEST_PATH)) == len(original_items) + 1
+    # Check: Check if split dir has the generated test/train/valid.
+    assert set(list_dirs(osp.join(TEST_PATH, COPY_FOLDER))) == set(GENERATED_DIRS) 
+    #after_split = [list_items(d) for d in os.list_dirs(osp.join(TEST_PATH, COPY_FOLDER))]
+    #assert after_split
+
 
 def test_split_move():
     # Tests split when moving files
-    pass
+    original_items = list_items(TEST_PATH)
+    sp.split(RATIO, TEST_PATH, False)
+    assert set(list_dirs(TEST_PATH)) == set(GENERATED_DIRS)
+
