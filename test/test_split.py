2c8af6c10c001e9aa2006e2e82d3705d46575297
Murilo Ferreira <fferreira.murilo@gmail.com>
2019-08-08 19:05:06 -0300

    Finishes tests for split.py (Closes #4)

--- test/test_split.py
+++ test/test_split.py
@@ -31,7 +31,14 @@ def list_items(path):
     return dict(zip(classes, items))
 
 def quantities_check(before, after):
-    pass
+    before_size = sum([len(item) for item in before.values()])
+    after_items = [list(dic.values()) for dic in after]
+    after_items = [y for x in after_items for y in x]
+    print(after_items)
+    after_size = sum([len(item) for item in after_items])
+    print(f"Before size: {before_size}")
+    print(f"After size: {after_size}")
+    return after_size == before_size
 
 def test_split_copy():
     # Tests split when copying files
@@ -43,13 +50,21 @@ def test_split_copy():
     assert len(list_dirs(TEST_PATH)) == len(original_items) + 1
     # Check: Check if split dir has the generated test/train/valid.
     assert set(list_dirs(osp.join(TEST_PATH, COPY_FOLDER))) == set(GENERATED_DIRS) 
-    #after_split = [list_items(d) for d in os.list_dirs(osp.join(TEST_PATH, COPY_FOLDER))]
-    #assert after_split
+    after_split = [list_items(osp.join(TEST_PATH, COPY_FOLDER, d)) 
+                   for d in GENERATED_DIRS]
+    # Check: Check if the after_split has the same quantities of items as
+    # original_items has
+    assert quantities_check(original_items, after_split)
 
 
 def test_split_move():
     # Tests split when moving files
     original_items = list_items(TEST_PATH)
     sp.split(RATIO, TEST_PATH, False)
+    # Check: if dataset dir only has the test/train/valid dirs. 
     assert set(list_dirs(TEST_PATH)) == set(GENERATED_DIRS)
-
+    after_split = [list_items(osp.join(TEST_PATH, d)) 
+                   for d in GENERATED_DIRS]
+    # Check: after_split has the same quantities of items as
+    # original_items has
+    assert quantities_check(original_items, after_split)
